c34bb57c2f522003110362f0214ca385d70d1f63
SunshineDN <sunshinedn2003@gmail.com>
2023-07-10 21:23:36 -0300

    Base url removido

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <BrowserRouter>
       <ThemeProvider theme={ theme }>
         <GlobalStyle />
         <App />
